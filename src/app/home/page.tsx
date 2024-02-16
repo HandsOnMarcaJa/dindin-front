47870e9854c622bac0973c95aa5afd77506747c6
Daiane Bolzan <dainha85@gmail.com>
2024-02-16 18:00:14 -0300

    feat: adjust navbar and update home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,39 +1,10 @@
-'use client'
-import EditModal from '@/components/EditModal'
-import Image from 'next/image'
-import { useState } from 'react'
-import Person from '@/assets/person.svg'
-import logoutImg from '@/assets/logout.svg'
-import { RegisterForm } from '@/components/RegisterForm'
+import Navbar from '@/components/Navbar'
 
 export default function HomePage() {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
-  const openModal = () => setIsModalOpen(true)
-  const closeModal = () => setIsModalOpen(false)
-  const logout = () => {
-    // Logout logic here
-  }
-
   return (
-    <div className="p-4">
-      <button className="px-4 py-2 rounded " onClick={openModal}>
-        <Image src={Person} alt={'profile pic'} />
-      </button>
-      <button className="px-4 py-2 rounded " onClick={logout}>
-        <Image src={logoutImg} alt={'door image'} />
-      </button>
-
-      <EditModal isOpen={isModalOpen} onClose={closeModal}>
-        <div className="p-4 text-black">
-          <RegisterForm
-            formName="Editar Perfil"
-            buttonName="Confirmar"
-            isRegistering={false}
-            onClose={closeModal}
-          />
-        </div>
-      </EditModal>
+    <div className="w-full h-full max-h-full max-w-[1270px] bg-white ">
+      <Navbar />
+      <h1 className="text-black">Aqui vão as transações</h1>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,56 @@
+'use client'
+import EditModal from '@/components/EditModal'
+import Image from 'next/image'
+import { useState } from 'react'
+import Person from '@/assets/person.svg'
+import logoutImg from '@/assets/logout.svg'
+import { RegisterForm } from '@/components/RegisterForm'
+import { useRouter } from 'next/navigation'
 import { Logo } from './Logo'
 
-export function Navbar() {
+export default function Navbar() {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const router = useRouter()
+
+  const openModal = () => setIsModalOpen(true)
+  const closeModal = () => setIsModalOpen(false)
+
+  const username = 'User'
+
+  const logout = () => {
+    router.push('/login')
+    // Logout logic here
+  }
+
   return (
-    <div className="bg-gradiente">
-      <Logo />
-      <div></div>
+    <div>
+      <div className="bg-custom-gradient p-4 flex flex-row justify-between">
+        <Logo />
+        <div className="flex items-center">
+          <button
+            className="px-4 flex items-center py-2 rounded "
+            onClick={openModal}
+          >
+            <Image src={Person} alt={'profile pic'} />
+            <h1>{username}</h1>
+          </button>
+
+          <button className="px-4 py-2 rounded " onClick={logout}>
+            <Image src={logoutImg} alt={'door image'} />
+          </button>
+        </div>
+
+        <EditModal isOpen={isModalOpen} onClose={closeModal}>
+          <div className="p-4 text-white">
+            <RegisterForm
+              formName="Editar Perfil"
+              buttonName="Confirmar"
+              isRegistering={false}
+              onClose={closeModal}
+            />
+          </div>
+        </EditModal>
+      </div>
     </div>
   )
 }
