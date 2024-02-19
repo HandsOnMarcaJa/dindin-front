d770117f75c6061d85b69960b8fc268f8b5a2f7e
JPDovale <joaopaulodovaledeo.60@gmail.com>
2024-02-19 15:29:37 -0300

    feat: add mock of transaction

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,21 @@
 import Navbar from '@/components/Navbar'
+import Image from 'next/image'
 
 export default function HomePage() {
   return (
-    <div className="w-full h-full max-h-full max-w-[1270px] bg-zinc-100 ">
+    <section className="flex flex-col min-h-screen gap-4 bg-zinc-100">
       <Navbar />
-      <h1 className="text-black">Aqui vão as transações</h1>
-    </div>
+      <div className="w-full mx-auto mt-3 max-w-[1270px] ">
+        <Image
+          className="w-full object-contain"
+          src="/mock.png"
+          alt=""
+          width={1200}
+          height={800}
+          quality={100}
+          priority
+        />
+      </div>
+    </section>
   )
 }
